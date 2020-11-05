27d6aade68e61814e8c224e7566c6157f170ef32
stevenburton <stevenburton@digital.uc.dwp.gov.uk>
2020-11-05 16:08:00 +0000

    Add audit monitoring and dashboard

--- k2hb_dashboard.tf
+++ k2hb_dashboard.tf
@@ -37,3 +37,23 @@ resource "aws_cloudwatch_dashboard" "k2hb_equality" {
 
   })
 }
+
+resource "aws_cloudwatch_dashboard" "k2hb_audit" {
+  dashboard_name = "kafka-to-hbase-audit"
+  dashboard_body = templatefile("${path.module}/dashboards/kafka-to-hbase.json.tpl", {
+    namespace                                                    = local.cw_k2hb_audit_agent_namespace
+    reconciliation_namespace                                     = data.terraform_remote_state.ingest.outputs.locals.cw_k2hb_reconciliation_audit_namespace
+    log_group                                                    = local.ingest_log_groups.k2hb_ec2_audit_logs.name
+    k2hb_metric_name_number_of_successfully_processed_records    = local.k2hb_metric_name_number_of_successfully_processed_records
+    k2hb_metric_name_speed_of_successfully_processed_batches     = local.k2hb_metric_name_speed_of_successfully_processed_batches
+    k2hb_metric_name_number_of_successfully_processed_batches    = local.k2hb_metric_name_number_of_successfully_processed_batches
+    k2hb_metric_name_messages_written_to_dlq                     = local.k2hb_metric_name_messages_written_to_dlq
+    k2hb_metric_name_timeouts_reading_kafka                      = local.k2hb_metric_name_timeouts_reading_kafka
+    k2hb_metric_name_failures_writing_hbase                      = local.k2hb_metric_name_failures_writing_hbase
+    k2hb_metric_name_failed_batches                              = local.k2hb_metric_name_failed_batches
+    reconciliation_metric_name_successfully_reconciled           = data.terraform_remote_state.ingest.outputs.locals.k2hb_reconciliation_metric_name_number_of_successfully_reconciled_records["audit_reconciliation"]
+    reconciliation_metric_name_failed_to_be_reconciled           = data.terraform_remote_state.ingest.outputs.locals.k2hb_reconciliation_metric_name_number_of_records_which_failed_reconciliation["audit_reconciliation"]
+    number_of_unreconciled_records_after_specified_age_alarm_arn = data.terraform_remote_state.ingest.outputs.metrics.number_of_unreconciled_records_after_specified_age_alarm.audit.arn
+
+  })
+}
