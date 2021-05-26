ed6d223e6942beb50a89a8f9c17aae8411206d68
stevenburton <stevenburton@digital.uc.dwp.gov.uk>
2021-05-26 12:50:59 +0100

    Use outputs

--- k2hb_reconciliation_trimmer.tf
+++ k2hb_reconciliation_trimmer.tf
@@ -2,6 +2,35 @@ locals {
   k2hb_reconciliation_trimmer_image_url = "${local.account.management}.${module.vpc.ecr_dkr_domain_name}/kafka-to-hbase-reconciliation${var.k2hb_reconciliation_container_version}"
 }
 
+resource "aws_iam_role" "ecs_instance_role_k2hb_reconciliation_trimmer_batch" {
+  name = "ecs_instance_role_k2hb_reconciliation_trimmer_batch"
+
+  assume_role_policy = <<EOF
+{
+    "Version": "2012-10-17",
+    "Statement": [
+      {
+        "Action": "sts:AssumeRole",
+        "Effect": "Allow",
+        "Principal": {
+          "Service": "ec2.amazonaws.com"
+        }
+      }
+    ]
+}
+EOF
+
+  tags = merge(
+    local.common_tags,
+    { Name = "k2hb_reconciliation_trimmer" }
+  )
+}
+
+resource "aws_iam_instance_profile" "ecs_instance_role_k2hb_reconciliation_trimmer_batch" {
+  name = "ecs_instance_role_k2hb_reconciliation_trimmer_profile"
+  role = aws_iam_role.ecs_instance_role_k2hb_reconciliation_trimmer_batch.name
+}
+
 resource "aws_batch_compute_environment" "k2hb_reconciliation_trimmer_batch" {
   compute_environment_name = "k2hb_reconciliation_trimmer"
   service_role             = data.aws_iam_role.aws_batch_service_role.arn
@@ -16,8 +45,8 @@ resource "aws_batch_compute_environment" "k2hb_reconciliation_trimmer_batch" {
     desired_vcpus = 0
     max_vcpus     = 8
 
-    security_group_ids = [aws_security_group.k2hb_reconciliation_trimmer_batch.id]
-    subnets            = aws_subnet.business_data.*.id
+    security_group_ids = [data.terraform_remote_state.ingest.outputs.ingestion_vpc.vpce_security_groups.k2hb_reconciliation_trimmer_batch.id]
+    subnets            = data.terraform_remote_state.ingest.outputs.ingestion_subnets.id
     type               = "EC2"
 
     tags = merge(
